f3ea31012553269a58c6276738b982556b304a35
Igbinosun E. Kendrick <eki318@student.bham.ac.uk>
2026-07-08 20:23:55 +0100

    Git commit implementation

--- main.py
+++ main.py
@@ -52,3 +52,31 @@ def read_index():
         i += entry_len
     assert len(entries) == num_entries
     return entries
+
+def commit(message, author):
+    """Commit the current state of the index to master with given message.
+    Return hash of commit object.
+    """
+    tree = write_tree()
+    parent = get_local_master_hash()
+    timestamp = int(time.mktime(time.localtime()))
+    utc_offset = -time.timezone
+    author_time = '{} {}{:02}{:02}'.format(
+            timestamp,
+            '+' if utc_offset > 0 else '-',
+            abs(utc_offset) // 3600,
+            (abs(utc_offset) // 60) % 60)
+    lines = ['tree ' + tree]
+    if parent:
+        lines.append('parent ' + parent)
+    lines.append('author {} {}'.format(author, author_time))
+    lines.append('committer {} {}'.format(author, author_time))
+    lines.append('')
+    lines.append(message)
+    lines.append('')
+    data = '\n'.join(lines).encode()
+    sha1 = hash_object(data, 'commit')
+    master_path = os.path.join('.git', 'refs', 'heads', 'master')
+    write_file(master_path, (sha1 + '\n').encode())
+    print('committed to master: {:7}'.format(sha1))
+    return sha1
