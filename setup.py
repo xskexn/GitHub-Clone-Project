224bbb19f652e9e666cbaa0b9689f948d6d16b07
Igbinosun E. Kendrick <eki318@student.bham.ac.uk>
2026-07-13 18:21:09 +0100

    Initial Pygit commit, with initialisation of the CLI and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name = 'pygit',
+       version = '1.0',
+       packages = ['pygit'],
+       entry_points = {
+           'console_scripts' : [
+               'pygit = pygit.cli:main'
+           ]
+       })
