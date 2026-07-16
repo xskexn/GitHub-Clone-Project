2b5536dab257dd17054d8c2b51d207e19c074379
Kendrick <info.kendrickigb@gmail.com>
2026-07-16 20:22:44 +0100

    Log feature implementation

--- pygit/base.py
+++ pygit/base.py
@@ -1,7 +1,10 @@
 import os
-from pathlib import Path
+import itertools
+import operator
 
+from pathlib import Path
 from . import data
+from collections import namedtuple
 
 def write_tree (directory='.'):
     entries = []
@@ -10,73 +13,73 @@ def write_tree (directory='.'):
         for entry in it:
             full = os.path.join(directory, entry.name)
             # Checks if item is something in the 'gitIgnore' and just skips them (like .pygit directory)
-            if is_ignored (full):
+            if is_ignored(full):
                 continue
             # file found in the search after exploring the subfolders
-            if entry.is_file (follow_symlinks=False):
+            if entry.is_file(follow_symlinks=False):
                 type_ = 'blob'
                 # reading the contents
                 with open (full, 'rb') as f:
                     # feeds it into the object hash function to get the oid (unique hash)
-                    oid = data.hash_object (f.read ())
-            elif entry.is_dir (follow_symlinks=False):
+                    oid = data.hash_object(f.read ())
+            elif entry.is_dir(follow_symlinks=False):
                 type_ = 'tree'
                 # this process is done recusively and called on all the subfolders until a file is reached
-                oid = write_tree (full)
+                oid = write_tree(full)
                 # tree building step where all entires are sorted alphabethically into a strucutred plain text string
-            entries.append ((entry.name, oid, type_))
+            entries.append((entry.name, oid, type_))
 
     tree = ''.join (f'{type_} {oid} {name}\n'
                     for name, oid, type_
                     in sorted (entries))
-    return data.hash_object (tree.encode (), 'tree')
+    return data.hash_object(tree.encode (), 'tree')
 
-def _iter_tree_entries (oid):
+def _iter_tree_entries(oid):
     if not oid:
         return
-    tree = data.get_object (oid, 'tree')
-    for entry in tree.decode ().splitlines ():
-        type_, oid, name = entry.split (' ', 2)
+    tree = data.get_object(oid, 'tree')
+    for entry in tree.decode().splitlines():
+        type_, oid, name = entry.split(' ', 2)
         yield type_, oid, name
 
-def get_tree (oid, base_path=''):
+def get_tree(oid, base_path=''):
     result = {}
-    for type_, oid, name in _iter_tree_entries (oid):
+    for type_, oid, name in _iter_tree_entries(oid):
         assert '/' not in name
         assert name not in ('..', '.')
         path = base_path + name
         if type_ == 'blob':
             result[path] = oid
         elif type_ == 'tree':
-            result.update (get_tree (oid, f'{path}/'))
+            result.update(get_tree (oid, f'{path}/'))
         else:
             assert False, f'Unknown tree entry {type_}'
     return result
 
-def _empty_current_directory ():
+def _empty_current_directory():
     for root, dirnames, filenames in os.walk ('.', topdown=False):
         for filename in filenames:
             path = os.path.relpath (f'{root}/{filename}')
-            if is_ignored (path) or not os.path.isfile (path):
+            if is_ignored(path) or not os.path.isfile (path):
                 continue
-            os.remove (path)
+            os.remove(path)
         for dirname in dirnames:
             path = os.path.relpath (f'{root}/{dirname}')
-            if is_ignored (path):
+            if is_ignored(path):
                 continue
             try:
-                os.rmdir (path)
+                os.rmdir(path)
             except (FileNotFoundError, OSError):
                 # Deletion might fail if the directory contains ignored files,
                 # so it's OK
                 pass
 
 def read_tree (tree_oid):
-    _empty_current_directory ()
-    for path, oid in get_tree (tree_oid, base_path='./').items ():
-        os.makedirs (os.path.dirname (path), exist_ok=True)
+    _empty_current_directory()
+    for path, oid in get_tree(tree_oid, base_path='./').items():
+        os.makedirs(os.path.dirname (path), exist_ok=True)
         with open (path, 'wb') as f:
-            f.write (data.get_object (oid))
+            f.write(data.get_object (oid))
 
 def commit (message):
     commit = f'tree {write_tree ()}\n'
@@ -85,12 +88,28 @@ def commit (message):
         commit += f'parent {HEAD}\n'
     commit += '\n'
     commit += f'{message}\n'
-
-    oid = data.hash_object (commit.encode (), 'commit')
-
-    data.set_HEAD (oid)
-
+    oid = data.hash_object(commit.encode(), 'commit')
+    data.set_HEAD(oid)
     return oid
 
-def is_ignored (path):
+Commit = namedtuple('Commit', ['tree', 'parent', 'message'])
+
+def get_commit(oid):
+    parent = None
+
+    commit = data.get_object (oid, 'commit').decode()
+    lines = iter (commit.splitlines())
+    for line in itertools.takewhile(operator.truth, lines):
+        key, value = line.split(' ', 1)
+        if key == 'tree':
+            tree = value
+        elif key == 'parent':
+            parent = value
+        else:
+            assert False, f'Unknown field {key}'
+
+    message = '\n'.join(lines)
+    return Commit(tree=tree, parent=parent, message=message)
+
+def is_ignored(path):
     return '.pygit' in Path(path).parts
--- pygit/cli.py
+++ pygit/cli.py
@@ -1,65 +1,87 @@
 import argparse
+import textwarp
 import os
 import sys
 
 from . import data
 from . import base
 
-def main ():
-    args = parse_args ()
-    args.func (args)
+def main():
+    args = parse_args()
+    args.func(args)
 
-def parse_args ():
-    parser = argparse.ArgumentParser ()
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-    commands = parser.add_subparsers (dest='command')
+    commands = parser.add_subparsers(dest='command')
     commands.required = True
 
-    init_parser = commands.add_parser ('init')
-    init_parser.set_defaults (func=init)
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
 
-    hash_object_parser = commands.add_parser ('hash-object')
-    hash_object_parser.set_defaults (func=hash_object)
-    hash_object_parser.add_argument ('file')
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
 
     #Creating the subcommand name
-    cat_file_parser = commands.add_parser ('cat-file')
+    cat_file_parser = commands.add_parser('cat-file')
     # Attaches the command to the actual python function
-    cat_file_parser.set_defaults (func=cat_file)
+    cat_file_parser.set_defaults(func=cat_file)
     # Registering the expected variable
-    cat_file_parser.add_argument ('object')
+    cat_file_parser.add_argument('object')
 
-    write_tree_parser = commands.add_parser ('write-tree')
-    write_tree_parser.set_defaults (func=write_tree)
+    write_tree_parser = commands.add_parser('write-tree')
+    write_tree_parser.set_defaults(func=write_tree)
 
-    read_tree_parser = commands.add_parser ('read-tree')
-    read_tree_parser.set_defaults (func=read_tree)
-    read_tree_parser.add_argument ('tree')
+    read_tree_parser = commands.add_parser('read-tree')
+    read_tree_parser.set_defaults(func=read_tree)
+    read_tree_parser.add_argument('tree')
 
-    commit_parser = commands.add_parser ('commit')
-    commit_parser.set_defaults (func=commit)
-    commit_parser.add_argument ('-m', '--message', required=True)
+    commit_parser = commands.add_parser('commit')
+    commit_parser.set_defaults(func=commit)
+    commit_parser.add_argument('-m', '--message', required=True)
 
-    return parser.parse_args ()
+    log_parser = commands.add_parser('log')
+    log_parser.set_defaults(func=log)
+    log_parser.add_argument ('oid', nargs='?')
 
-def init (args):
-    data.init ()
+    return parser.parse_args()
+
+# Initialisation command creates a folder to store structural data
+def init(args):
+    data.init()
     print (f'Initialised empty pygit repository in {os.getcwd()}/{data.GIT_DIR}')
 
-def hash_object (args):
-    with open (args.file, 'rb') as f:
-        print (data.hash_object (f.read ()))
+# Compresses target file into 40-char sha-1 crypto file and saves into object folder
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object (f.read ()))
 
+# Retrives content of the file by feeding it the 40-char address
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object (args.object, expected=None))
 
-def cat_file (args):
-    sys.stdout.flush ()
-    sys.stdout.buffer.write (data.get_object (args.object, expected=None))
+# Takes a snapshot of the current working directory 
+def write_tree(args):
+    print(base.write_tree())
 
-def write_tree (args):
-    print(base.write_tree ())
+# Restores the project files to previously saved snapshots
+def read_tree(args):
+    base.read_tree(args.tree)
 
-def read_tree (args):
-    base.read_tree (args.tree)
+# Creates a new object that stores all the information like author, time and hash address in a key value format
+def commit(args):
+    print(base.commit(args.message))
 
-def commit (args):
-    print (base.commit (args.message))
+# Walks the list of commits and prints them 
+def log(args):
+    oid = args.oid or data.get_HEAD ()
+    while oid:
+        commit = base.get_commit(oid)
+
+        print(f'commit {oid}\n')
+        print(textwrap.indent(commit.message, '    '))
+        print('')
+
+        oid = commit.parent
