bed10a9deb9f2358396d11b8432d22c18fd176e5
Kendrick <info.kendrickigb@gmail.com>
2026-07-17 22:17:38 +0100

    Extended object id search fields

--- pygit/base.py
+++ pygit/base.py
@@ -120,7 +120,22 @@ def create_tag(name, oid):
     data.update_ref (f'refs/tags/{name}', oid)
 
 def get_oid(name):
-    return data.get_ref(name) or name
+    refs_to_try = [
+        f'{name}',
+        f'refs/{name}',
+        f'refs/tags/{name}',
+        f'refs/heads/{name}',
+    ]
+    for ref in refs_to_try:
+        if data.get_ref(ref):
+            return data.get_ref(ref)
+
+    # Name is SHA1
+    is_hex = all (c in string.hexdigits for c in name)
+    if len(name) == 40 and is_hex:
+        return name
+
+    assert False, f'Unknown name {name}'
 
 def is_ignored(path):
     return '.pygit' in Path(path).parts
